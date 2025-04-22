8efd25f0a01928e4546bba24a0183d843cf90d1b
AnkhaNav <neutralucet@email.cz>
2025-04-22 20:58:41 +0200

    Update goodbye.php

--- goodbye.php
+++ goodbye.php
@@ -9,10 +9,11 @@
 <body>
     <?php include 'header.php'; ?>
     <main>
-        <h1>Účet byl úspěšně odstraněn</h1>
-        <p>Je nám líto, že odcházíte. Pokud si to rozmyslíte, můžete se kdykoliv zaregistrovat znovu.</p>
-        <a href="index.php">Zpět na hlavní stránku</a>
-    </main>
+        <div class="form">
+            <h1>Účet byl úspěšně odstraněn</h1>
+            <p>Je nám líto, že odcházíte. Pokud si to rozmyslíte, můžete se kdykoliv zaregistrovat znovu.</p>
+            <a href="index.php" class="back-link">Zpět na hlavní stránku</a>
+        </main>
     <?php include 'footer.php'; ?>
 </body>
 </html>
