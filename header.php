8188bad6dcd5117d845e2b9b59a4af93e55daf58
AnkhaNav <neutralucet@email.cz>
2025-04-22 20:59:19 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,10 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 ?>
+<?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
+    <a href="admin_dashboard.php">Admin</a>
+<?php endif; ?>
+
 <header class="main-header">
     <div class="logo">
         <a href="index.php">3D Printing Services Brint</a>
@@ -14,7 +18,11 @@ if (session_status() === PHP_SESSION_NONE) {
             <li><a href="form.php">Objednávka</a></li>
             <li><a href="calculator.php">Kalkulačka</a></li>
             <?php if (isset($_SESSION['user_id'])): ?>
-                <li><a href="account.php">Můj účet</a></li>
+                <?php if (!empty($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
+                    <li><a href="admin_dashboard.php">Spravovat</a></li>
+                <?php else: ?>
+                    <li><a href="account.php">Můj účet</a></li>
+                <?php endif; ?>
                 <li><a href="logout.php">Odhlásit se</a></li>
             <?php else: ?>
                 <li><a href="login.php">Přihlásit se</a></li>
@@ -22,3 +30,13 @@ if (session_status() === PHP_SESSION_NONE) {
         </ul>
     </nav>
 </header>
+
+<?php if (isset($_SESSION['user_id'])): ?>
+    <?php if (!empty($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
+        <a href="admin_dashboard.php">Spravovat</a>
+    <?php else: ?>
+        <a href="account.php">Můj účet</a>
+    <?php endif; ?>
+<?php else: ?>
+    <a href="login.php">Přihlásit se</a>
+<?php endif; ?>
